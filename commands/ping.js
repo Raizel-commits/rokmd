7265e01d5a1ff68f78acf7198422728ca05dc1d8
Raizel-commits <djinoutcheu@gmail.com>
2025-12-29 05:22:21 +0100

    Update ping.js

--- commands/ping.js
+++ commands/ping.js
@@ -1,11 +1,6 @@
 export default {
     name: "ping",
-    description: "Répond Pong!",
-    async execute(sock, msg, args, commands) {
-        try {
-            await sock.sendMessage(msg.key.remoteJid, { text: "Pong!" });
-        } catch (err) {
-            console.error("Erreur dans la commande ping:", err);
-        }
+    execute: async (sock, m) => {
+        m.reply("pong 🏓");
     }
 };

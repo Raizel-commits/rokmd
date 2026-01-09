77356b34d376db74314fb35586be7ba758197e6e
Raizel-commits <djinoutcheu@gmail.com>
2026-01-09 12:00:08 +0100

    Create User.js

---
+++ User.js
@@ -0,0 +1,23 @@
+// User.js
+import mongoose from "mongoose";
+import bcrypt from "bcryptjs";
+
+const userSchema = new mongoose.Schema({
+  email: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  coins: { type: Number, default: 20 }, // coins gratuits à l'inscription
+  createdAt: { type: Date, default: Date.now }
+});
+
+userSchema.pre("save", async function(next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function(candidatePassword) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);

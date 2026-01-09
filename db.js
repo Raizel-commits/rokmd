ce7c9010857188b15f0f094b74d87b606202a9ad
Raizel-commits <djinoutcheu@gmail.com>
2026-01-09 12:51:21 +0100

    Update db.js

--- db.js
+++ db.js
@@ -1,11 +1,18 @@
+// db.js
 import mongoose from "mongoose";
 
-export const connectDB = async () => {
+const MONGO_URI =
+  "mongodb+srv://rokxd_raizel:Sangoku77@cluster0.0g3b0yp.mongodb.net/?appName=Cluster0";
+
+export async function connectDB() {
   try {
-    await mongoose.connect(process.env.MONGO_URI);
-    console.log("✅ MongoDB connecté");
+    await mongoose.connect(MONGO_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("✅ MongoDB connectée !");
   } catch (err) {
     console.error("❌ Erreur MongoDB :", err.message);
     process.exit(1);
   }
-};
+}
